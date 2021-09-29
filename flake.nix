7ff2c2589f42f7b70ecc7cda7e073bc115ef3761
mhanggi <29100324+mhanggi@users.noreply.github.com>
2021-09-29 16:17:23 +0200

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -2,22 +2,36 @@
   description = "myrmidon - task launcher using rofi";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux" "aarch64-linux"] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        packageName = "myrmidon";
-      in {
-        packages.${packageName} = pkgs.runCommandLocal "myrmidon" {
-          script = ./myrmidon.sh;
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-        } ''
-          makeWrapper $script $out/bin/myrmidon.sh \
-          --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.inotify-tools pkgs.libnotify ]}
-        '';
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config = { allowUnfree = "true";};
+    };
+  in rec {
+    packages.x86_64-linux.myrmidon = pkgs.runCommandLocal "myrmidon" {
+      script = ./myrmidon.sh;
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+    } ''
+      makeWrapper $script $out/bin/myrmidon \
+      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.inotify-tools pkgs.libnotify ]}
+    '';
 
-        defaultPackage = self.packages.${system}.${packageName}; 
-      });
+    defaultPackage.x86_64-linux = packages.x86_64-linux.myrmidon;
+
+    overlay = (final: prev: {
+      myrmidon = packages.x86_64-linux.myrmidon;
+    });
+
+    apps.x86_64-linux = {
+      myrmidon = {
+        type = "app";
+        program = "${defaultPackage}/result/myrmidon.sh";
+      };
+    };
+
+
+    defaultApp = apps.x86_64-linux.myrmidon;
+  };
 }
